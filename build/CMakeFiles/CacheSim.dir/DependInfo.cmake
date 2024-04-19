aa6e5d9f9ae9b5c9ed2e8f0b649ee6942542fc1c
SP4595 <109747786+SP4595@users.noreply.github.com>
2024-04-19 15:03:39 +0800

    initilize

---
+++ build/CMakeFiles/CacheSim.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/Desktop/CSC3050 HW03/Project 3/src/Cache.cpp" "CMakeFiles/CacheSim.dir/src/Cache.cpp.o" "gcc" "CMakeFiles/CacheSim.dir/src/Cache.cpp.o.d"
+  "/home/ubuntu/Desktop/CSC3050 HW03/Project 3/src/MainSinCache.cpp" "CMakeFiles/CacheSim.dir/src/MainSinCache.cpp.o" "gcc" "CMakeFiles/CacheSim.dir/src/MainSinCache.cpp.o.d"
+  "/home/ubuntu/Desktop/CSC3050 HW03/Project 3/src/MemoryManager.cpp" "CMakeFiles/CacheSim.dir/src/MemoryManager.cpp.o" "gcc" "CMakeFiles/CacheSim.dir/src/MemoryManager.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
